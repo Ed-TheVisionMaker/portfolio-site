c834968e1a0aa29e7a4c937b950c62a5ac7abb35
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-03-23 10:53:22 +0000

    added banner image and links

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,4 @@
-import LinkedInBanner from './assets/LinkedIn-Banner.png';
+import LinkedInBanner from '../../assets/linked-in-banner.png';
 
 export default function Hero() {
   return (
@@ -29,14 +29,16 @@ export default function Hero() {
                 <p className='mb-4'>Get to know me better</p>
                 <div className='flex items-center justify-center gap-x-6'>
                   <a
-                    href='#'
+                    href='https://github.com/Ed-TheVisionMaker'
                     className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                    target='_blank'
                   >
                     Git Hub
                   </a>
                   <a
-                    href='#'
+                    href='https://www.linkedin.com/in/ehoulcroft/'
                     className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                    target='_blank'
                   >
                     LinkedIn
                   </a>
@@ -46,8 +48,8 @@ export default function Hero() {
             <div className='mt-16 flow-root sm:mt-24'>
               <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                 <img
-                  src='LinkedInBanner'
-                  alt='App screenshot'
+                  src={LinkedInBanner}
+                  alt='LinkedInBanner'
                   width={2432}
                   height={1442}
                   className='rounded-md shadow-2xl ring-1 ring-gray-900/10'
